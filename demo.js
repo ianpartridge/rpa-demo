c3ba068a2260bc2a4a0366b24a863a46ed913390
Ian Partridge <i.partridge@uk.ibm.com>
2022-05-24 10:33:02 +0100

    Updates

--- demo.js
+++ demo.js
@@ -1,9 +1,15 @@
+const jobRoles = ['Manager', 'Developer', 'Analyst'];
+const numberOfRoles = jobRoles.length;
+
 function doResults() {
     var resultsDiv = document.getElementById("resultsDiv");
     resultsDiv.style.display = "none";
 
     var employeeName = document.getElementById("employeeId");
-    employeeName.innerHTML = document.getElementById("employeeIdInput").value
+    employeeName.innerHTML = document.getElementById("employeeIdInput").value;
+
+    var jobRole = document.getElementById("jobRole");
+    jobRole.innerHTML = jobRoles[Math.floor(Math.random() * numberOfRoles)];
 
     var hoursWorked = document.getElementById("hoursWorked");
     const hours = Math.floor(Math.random() * 37);
